757cf2938e3b9132ad1a73f37c05941c6f785687
ihassan0 <hassanbabar852629@gmail.com>
2024-07-03 08:06:41 -0700

    Signup pagge validation.

--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -9,6 +9,9 @@ export default function Signup() {
     const [email, SetEmail] = useState()
     const [location, SetLocation] = useState()
     const[password, SetPassword] = useState()
+    const [errorMessageName, setErrorMessageName] = useState('');
+    const [errorMessageEmail, setErrorMessageEmail] = useState('');
+    const [errorMessagePass, setErrorMessagePass] = useState('');
     const navigate = useNavigate()
     const handleformSubmit = (e) => {
         e.preventDefault();
@@ -17,6 +20,25 @@ export default function Signup() {
         })
         .then(result => {console.log(result)
             navigate('/login')
+        })
+        .catch(err => {
+            // console.log(err.response.data.errors)
+            if (err.response.data.errors) {
+                console.log('hi')
+                // Map errors to an object
+                const errorObj = err.response.data.errors.reduce((acc, error) => {
+                    // console.log(acc)
+                    acc[error.path] = error.msg;
+                    return acc;
+                }, {});
+                setErrorMessageName(errorObj.name);
+                setErrorMessageEmail(errorObj.email);
+                setErrorMessagePass(errorObj.password);
+                console.log(errorObj.email)
+            } else {
+                console.log("In");
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
         })
         
     }
@@ -34,12 +56,14 @@ export default function Signup() {
                                     <input type="text" className="form-control"name='name' required  id="name" placeholder="Enter your name"
                                     onChange={ (e) => SetName(e.target.value) } 
                                     />
+                                    {errorMessageName && <p className='text-danger'>{errorMessageName}</p>}
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="email">Email address</label>
-                                    <input type="email" className="form-control" id="email" required  placeholder="Enter your email"
+                                    <input type="text" className="form-control" id="email" required  placeholder="Enter your email"
                                     onChange={ (e) => SetEmail(e.target.value)}
                                      />
+                                     {errorMessageEmail && <p className='text-danger'>{errorMessageEmail}</p>}
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="name">Location</label>
@@ -52,6 +76,7 @@ export default function Signup() {
                                     <input type="password" className="form-control" id="password"  required placeholder="Enter your password"
                                     onChange={ (e) => SetPassword(e.target.value)}
                                     />
+                                    {errorMessagePass && <p className='text-danger'>{errorMessagePass}</p>}
                                 </div>
                                 <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
                                 <Link to='/login' className='m-3 btn btn-danger'>Login</Link>
