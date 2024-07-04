2f3ea53b08495ba7924308d01d672c945b28dc35
ihassan0 <hassanbabar852629@gmail.com>
2024-07-04 08:15:29 -0700

    myOrder page.

--- src/screens/MyOrder.jsx
+++ src/screens/MyOrder.jsx
@@ -20,7 +20,8 @@ export default function MyOrder() {
             })
         }).then(async (res) => {
             let response = await res.json()
-            await setorderData(response)
+            console.log(response.orderData.order_data)
+            await setorderData(response.orderData.order_data.flat())
         })
 
 
@@ -45,48 +46,28 @@ export default function MyOrder() {
             <div className='container'>
                 <div className='row'>
 
-                    {orderData.length !== 1 ? Array(orderData).map(data => {
+                    {orderData.length !== 0 ? orderData.map(data => {
+                        console.log( data.name);
                         return (
-                            data.orderData ?
-                                data.orderData.order_data.slice(0).reverse().map((item) => {
-                                    return (
-                                        item.map((arrayData) => {
-                                            return (
-                                                <div  >
-                                                    {arrayData.Order_date ? <div className='m-auto mt-5'>
+                            <div className='col-12 col-md-6 col-lg-3' >
+                            <div className="card mt-3" style={{ width: "16rem", maxHeight: "360px" }}>
+                                <img src={data.img} className="card-img-top" alt="..." style={{ height: "120px", objectFit: "fill" }} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{data.name}</h5>
+                                    <div className='container w-100 p-0' style={{ height: "38px" }}>
+                                        <span className='m-1'>{data.qty}</span>
+                                        <span className='m-1'>{data.size}</span>
+                                        {/* <span className='m-1'>{data}</span> */}
+                                        <div className=' d-inline ms-2 h-100 w-20 fs-5' >
+                                            Rs:{data.price}/-
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
 
-                                                        {data = arrayData.Order_date}
-                                                        <hr />
-                                                    </div> :
+                        </div>
+                           
 
-                                                        <div className='col-12 col-md-6 col-lg-3' >
-                                                            <div className="card mt-3" style={{ width: "16rem", maxHeight: "360px" }}>
-                                                                <img src={arrayData.img} className="card-img-top" alt="..." style={{ height: "120px", objectFit: "fill" }} />
-                                                                <div className="card-body">
-                                                                    <h5 className="card-title">{arrayData.name}</h5>
-                                                                    <div className='container w-100 p-0' style={{ height: "38px" }}>
-                                                                        <span className='m-1'>{arrayData.qty}</span>
-                                                                        <span className='m-1'>{arrayData.size}</span>
-                                                                        <span className='m-1'>{data}</span>
-                                                                        <div className=' d-inline ms-2 h-100 w-20 fs-5' >
-                                                                            Rs:{arrayData.price}/-
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-
-                                                        </div>
-
-
-
-                                                    }
-
-                                                </div>
-                                            )
-                                        })
-
-                                    )
-                                }) : ""
                         )
                     }) : ""}
                 </div>
